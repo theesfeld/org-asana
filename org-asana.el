44832ea8b07518ec721af79eb32a3458309ab838
theesfeld <william@theesfeld.net>
2025-07-17 10:17:18 -0400

    Add pure data fetching functions with rate limiting

--- org-asana.el
+++ org-asana.el
@@ -93,5 +93,80 @@
 (defvar org-asana--rate-limit-reset nil
   "Time when rate limit resets.")
 
+;;; API Request Functions
+
+(defun org-asana--check-rate-limit ()
+  "Check and enforce API rate limits."
+  (when (and org-asana--rate-limit-reset
+             (< org-asana--rate-limit-remaining 10)
+             (> (float-time org-asana--rate-limit-reset) (float-time)))
+    (let ((wait-time (- (float-time org-asana--rate-limit-reset) (float-time))))
+      (message "Rate limit low. Waiting %.1f seconds..." wait-time)
+      (sleep-for wait-time))))
+
+(defun org-asana--make-request (method endpoint &optional data)
+  "Make an API request to METHOD ENDPOINT with optional DATA."
+  (unless org-asana-token
+    (error "No Asana token configured. Set `org-asana-token'"))
+  (org-asana--check-rate-limit)
+  (let* ((url (concat org-asana-api-base-url endpoint))
+         (url-request-method method)
+         (url-request-extra-headers
+          `(("Authorization" . ,(concat "Bearer " org-asana-token))
+            ("Content-Type" . "application/json")))
+         (url-request-data (when data (json-encode data))))
+    (with-current-buffer (url-retrieve-synchronously url t t)
+      (goto-char (point-min))
+      (re-search-forward "^$")
+      (let ((response (json-read)))
+        (kill-buffer)
+        (sleep-for org-asana-rate-limit-delay)
+        response))))
+
+(defun org-asana--fetch-paginated (endpoint)
+  "Fetch all pages of data from ENDPOINT."
+  (let ((all-data '())
+        (next-page nil))
+    (catch 'done
+      (while t
+        (let* ((url (or next-page endpoint))
+               (response (org-asana--make-request "GET" url)))
+          (setq all-data (append all-data (alist-get 'data response)))
+          (setq next-page (alist-get 'next_page response))
+          (unless next-page (throw 'done all-data)))))))
+
+(defun org-asana--fetch-workspace-info ()
+  "Fetch current user and workspace information."
+  (let ((response (org-asana--make-request "GET" "/users/me")))
+    (let* ((user-data (alist-get 'data response))
+           (workspaces (alist-get 'workspaces user-data)))
+      (list (alist-get 'gid user-data)
+            (alist-get 'gid (car workspaces))))))
+
+(defun org-asana--fetch-all-tasks ()
+  "Fetch all incomplete tasks assigned to current user."
+  (let* ((workspace-info (org-asana--fetch-workspace-info))
+         (user-gid (car workspace-info))
+         (workspace-gid (cadr workspace-info))
+         (opt-fields (concat "gid,name,notes,completed,due_on,due_at,"
+                            "created_at,modified_at,created_by.name,"
+                            "memberships.project.gid,memberships.project.name,"
+                            "memberships.section.gid,memberships.section.name"))
+         (endpoint (format "/workspaces/%s/tasks/search?assignee.any=%s&completed=false&limit=100&opt_fields=%s"
+                          workspace-gid user-gid opt-fields)))
+    (org-asana--fetch-paginated endpoint)))
+
+(defun org-asana--fetch-task-metadata (task-id)
+  "Fetch stories and attachments for TASK-ID."
+  (when org-asana-fetch-metadata
+    (let ((stories (org-asana--make-request 
+                   "GET" 
+                   (format "/tasks/%s/stories" task-id)))
+          (attachments (org-asana--make-request
+                       "GET"
+                       (format "/tasks/%s/attachments" task-id))))
+      (cons (alist-get 'data stories)
+            (alist-get 'data attachments)))))
+
 (provide 'org-asana)
 ;;; org-asana.el ends here
